1e59e405b800f4bdfbe522d0d671fa80b36c3800
ckim812 <109961170+ckim812@users.noreply.github.com>
2022-10-22 00:53:48 -0500

    added high scores section - not fully working yet

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,12 +2,27 @@ let time = 60;
 let quizRun = true;
 let clockId;
 let qI = 0;
+let score = 0;
+let quizDone = false;
+var allHighScores = [];
 const questionTitle = document.querySelector("#question-title");
 const questionText = document.querySelector("#question-text");
 const answerChoices = document.querySelector("#answer-choices");
 const takeQuizBtn = document.querySelector("#takeQuiz");
 const submitBtn = document.querySelector("#submit");
 const restartBtn = document.querySelector("#restart");
+const userScore = document.querySelector("#user-score");
+const totalScore = document.querySelector("#total-score");
+
+const init = () => {
+  submitBtn.style.display = "none";
+  restartBtn.style.display = "none";
+  document.getElementById("question-area").style.display = "none";
+  document.getElementById("message").style.display = "none";
+  document.getElementById("score").style.display = "none";
+  totalScore.innerHTML = questions.length;
+  document.getElementById("takeQuiz").addEventListener("click", takeQuiz);
+};
 
 const clock = () => {
   time--;
@@ -15,37 +30,55 @@ const clock = () => {
     clearInterval(clockId);
     time = 0;
   }
-  document.querySelector(".timer-count").innerHTML = time;
+  if (!quizDone) document.querySelector(".timer-count").innerHTML = time;
 };
 
 const handleAnswer = (correct, choice) => {
   if (choice == correct) {
-    console.log("Right!");
+    console.log("Correct!");
+    document.querySelector("#message").innerHTML = "<h2>Correct!</h2>";
+    score++;
   } else {
-    console.log("wrong!");
+    console.log("Incorrect!");
+    document.querySelector("#message").innerHTML = "<h2>Incorrect!</h2>";
   }
-  qI++;
-  showQuestion();
+  if (qI <= questions.length) {
+    qI++;
+    userScore.innerHTML = score;
+    showQuestion();
+    quizDone = false;
+  } else {
+    quizDone = true;
+  }
+  //   if (quizDone)
+};
+
+const displayHighScores = () => {
+    quizDone = true;
+
+  document.querySelector(".header").innerHTML = "<h1>Code Quiz</h1>";
+  let player = prompt("Please enter your name:");
+  questionTitle.textContent = "High Scores";
+  const highScore = [player, score, time];
+  if ((allHighScores.length === 0)) {
+    questionText.textContent = highScore;
+    allHighScores = highScore;
+} else {
+    const currentHighScores = localStorage.getItem("allHighScores");
+    allHighScores = currentHighScores.concat(currentHighScores);
+    questionText.textContent = allHighScores;
+}
+localStorage.setItem("allHighScores", allHighScores);
 };
 
 const showQuestion = () => {
   let { N, Q, A, C } = questions[qI];
   questionTitle.innerHTML = `<h1>${N}</h1>`;
   questionText.innerHTML = `<h2>${Q}</h2>`;
-  answerChoices.innerHTML = '';
+  answerChoices.innerHTML = "";
   A.forEach((answer) => {
     answerChoices.innerHTML += `<button class="btn" onclick="handleAnswer('${C}','${answer}')">${answer}</button>`;
   });
-};
-
-const hideButtons = (hide) => {
-  if (hide) {
-    submitBtn.style.display = "none";
-    restartBtn.style.display = "none";
-  } else {
-    submitBtn.style.display = "inline";
-    restartBtn.style.display = "inline";
-  }
 };
 
 const takeQuiz = () => {
@@ -54,10 +87,10 @@ const takeQuiz = () => {
   takeQuizBtn.style.display = "none";
   document.getElementById("info").style.display = "none";
   document.getElementById("question-area").style.display = "inline";
+  document.getElementById("message").style.display = "inline";
+  document.getElementById("score").style.display = "inline";
   showQuestion();
   quizRun = false;
 };
 
-hideButtons(true);
-document.getElementById("question-area").style.display = "none";
-document.getElementById("takeQuiz").addEventListener("click", takeQuiz);
+init();
